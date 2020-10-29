c465afd0acee748c8b20f59bed0f00a3984974f6
amarouane <amarouane@itsc.uah.edu>
2020-10-29 08:55:59 -0500

    Upgrade terraform to 0.12.26

--- requirements.sh
+++ requirements.sh
@@ -6,7 +6,7 @@ apt install -y nodejs
 
 
 # Install Terraform 0.12
-wget https://releases.hashicorp.com/terraform/0.12.16/terraform_0.12.16_linux_amd64.zip -O /usr/local/bin/terraform.zip 
+wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip -O /usr/local/bin/terraform.zip 
 unzip /usr/local/bin/terraform.zip -d /usr/local/bin/ 
 rm /usr/local/bin/terraform.zip
 
